df919359df2d2e53b6183369413b8b73b11287d3
이은덕 <pleed0215@ieundeog-ui-MacBookPro.local>
2020-11-05 21:23:12 +0900

    prisma settings....

--- dest/server.js
+++ dest/server.js
@@ -1,8 +1,15 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var graphql_yoga_1 = require("graphql-yoga");
-var typeDefs = "\n    type Query {\n        hello: String!   \n    }\n";
-var resolvers = {
-    hello: function () { return "Hi"; },
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var server = new graphql_yoga_1.GraphQLServer({ typeDefs: typeDefs, resolvers: resolvers });
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv").config();
+var graphql_yoga_1 = require("graphql-yoga");
+var morgan_1 = __importDefault(require("morgan"));
+var schema_1 = __importDefault(require("./schema"));
+var PORT = process.env.PORT || 4000;
+var server = new graphql_yoga_1.GraphQLServer({ schema: schema_1.default });
+server.express.use(morgan_1.default("dev"));
+server.start({ port: PORT }, function () {
+    return console.log("Server start with httP://127.0.0.1:" + PORT + ".");
+});
--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,44 @@ import logger from "morgan";
 
 import schema from "./schema";
 
+import { PrismaClient } from "@prisma/client";
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({ schema });
 
 server.express.use(logger("dev"));
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+      posts: {
+        create: { title: "Hello World" },
+      },
+      profile: {
+        create: { bio: "I like turtles" },
+      },
+    },
+  });
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+  console.dir(allUsers, { depth: null });
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
 
 server.start({ port: PORT }, () =>
   console.log(`Server start with httP://127.0.0.1:${PORT}.`)
